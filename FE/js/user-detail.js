c736248822b94cdc1a7729a63970662a6df761b9
Phuonghth <hhpcv7676@gmail.com>
2025-12-19 15:29:25 +0700

    add FE

--- FE/js/user-detail.js
+++
@@ -1,5 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get("id");
-
-const test = document.getElementById("test")
-test.innerText = "findbyid user = " + id
